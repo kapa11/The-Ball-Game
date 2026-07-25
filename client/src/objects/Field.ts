42b3f1305510891d9c9172aa16d496d6ae88b64a
Vihaan <vihaankashyap11@gmail.com>
2026-07-25 11:44:54 +0530

    added basic field code

---
+++ client/src/objects/Field.ts
@@ -0,0 +1,16 @@
+import { Graphics } from "pixi.js";
+
+export class Field extends Graphics {
+    static readonly WIDTH = 945;
+    static readonly HEIGHT = 612;
+
+    constructor() {
+        // Initialize the parent Graphics class.
+        super();
+
+        // Basic rounded rectangle
+        this
+            .roundRect(50, 50, Field.WIDTH, Field.HEIGHT, 30)
+            .fill({ color: 0x4c8527 });
+    }
+}
